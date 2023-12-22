106197e46787d04968ffe9e374c05c458ce5add4
NickN1994 <nickneggers@hotmail.com>
2023-12-22 12:34:20 +0100

    blogs worden opgehaald

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,37 +1,65 @@
 import posts from '../../constants/data.json';
 import {Link, useParams} from "react-router-dom";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './Blogs.css'
 import BlogListHelper from "./BlogListHelper.jsx";
+import axios from "axios";
 
 
 function Blogs() {
 
     const {id} = useParams();
+    const [getBlog, setGetblog] = useState();
+
+    useEffect(() => {
+        async function getBlogs() {
+            try {
+                const result = await axios.get('http://localhost:3000/posts', {
+                    headers: {
+                        Accept: 'application/json',
+                    }
+                });
+                console.log("gelukt")
+                console.log(result.data);
+                setGetblog(result.data);
+            } catch (e) {
+                console.error(e + "Het is niet gelukt om de blog op te halen");
+            }
+        }
+
+        getBlogs();
+    }, []);
 
 
     return (
+        <>
+            {
+                /*VRAAG
+            Ik heb nu wel alle blogs zichtbaar op de pagina.
+            Maar in de console worden de blogs wel telkens opnieuw geladen.
+            ook als ik eerst naar een andere pagina ga en weer terug naar de blog pagina,
+            dan wordt alles opnieuw geladen. Ik dacht dat met useEffect dit niet mogelijk was ?
 
-        <div className='outer-container'>
-            <div className='inner-container'>
 
-                {posts.map((post) => {
-                    return (
-                        <div className='blogs'>
-                            <BlogListHelper
-                                key={post.id}
-                                title={post.title}
+            */}
+            <div className='outer-container'>
+                <div className='inner-container'>
+                    {getBlog && getBlog.map((post) => {
+                        return (
+                            <div className='blogs'>
+                                <BlogListHelper
+                                    key={post.id}
+                                    title={post.title}
 
-                                author={post.author}
-                                id={post.id}
-                                comments={post.comments}
-                                shares={post.shares}
-                            /></div>)
-
-                })}
+                                    author={post.author}
+                                    id={post.id}
+                                    comments={post.comments}
+                                    shares={post.shares}
+                                /></div>)
+                    })}
+                </div>
             </div>
-        </div>
-
+        </>
     );
 }
 
